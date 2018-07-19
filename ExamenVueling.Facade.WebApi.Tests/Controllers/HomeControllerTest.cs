b36da0a04810d12dbe5c22ee53350dc1a49a5a18
Daniel Ruz <daniel.ruzvaz@gmail.com>
2018-07-19 22:55:04 +0200

    Initial DDD structure commit

---
+++ ExamenVueling.Facade.WebApi.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExamenVueling.Facade.WebApi;
+using ExamenVueling.Facade.WebApi.Controllers;
+
+namespace ExamenVueling.Facade.WebApi.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Disponer
+            HomeController controller = new HomeController();
+
+            // Actuar
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Declarar
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
---
+++ ExamenVueling.Facade.WebApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ExamenVueling.Facade.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}

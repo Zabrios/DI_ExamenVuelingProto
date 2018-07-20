d8fbe81f4f62593568f05256b659a0237a30a7f3
Daniel Ruz <daniel.ruzvaz@gmail.com>
2018-07-20 12:57:13 +0200

    Close #16

---
+++ ExamenVueling.Application.DTO/PolicyDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenVueling.Application.DTO
+{
+    public class PolicyDTO
+    {
+        public Guid Id { get; set; }
+        public double AmountInsured { get; set; }
+        public string Email { get; set; }
+        public DateTime InceptionDate { get; set; }
+        public bool InstallmentPayment { get; set; }
+        public Guid ClientId { get; set; }
+    }
+}
